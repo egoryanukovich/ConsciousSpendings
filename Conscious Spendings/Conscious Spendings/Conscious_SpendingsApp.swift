634964ef4da440ae9711a1d02cd156aa44055424
egor_yanukovich <egoryanukovich2@gmail.com>
2023-06-09 17:18:45 +0200

    added xcode project

---
+++ Conscious Spendings/Conscious Spendings/Conscious_SpendingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Conscious_SpendingsApp.swift
+//  Conscious Spendings
+//
+//  Created by Egor Yanukovich on 9.06.23.
+//
+
+import SwiftUI
+
+@main
+struct Conscious_SpendingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
